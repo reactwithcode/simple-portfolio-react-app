62dd6f964e0f4b4348866a5b42ecccd6d698be92
thohariakb <thohai.akbar@gmail.com>
2020-04-12 17:22:49 +0700

    Updates

---
+++ precache-manifest.0275242f15e58c026c2877184f8f27ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0f730c6de14ffc9f7a480dafb4c3f4",
+    "url": "/simple-portfolio-react-app/index.html"
+  },
+  {
+    "revision": "24a90ea581ed6ad034d4",
+    "url": "/simple-portfolio-react-app/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "018ac90983135bfa464e",
+    "url": "/simple-portfolio-react-app/static/css/main.0a291f73.chunk.css"
+  },
+  {
+    "revision": "24a90ea581ed6ad034d4",
+    "url": "/simple-portfolio-react-app/static/js/2.bc091ca1.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/simple-portfolio-react-app/static/js/2.bc091ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "018ac90983135bfa464e",
+    "url": "/simple-portfolio-react-app/static/js/main.1ffc8084.chunk.js"
+  },
+  {
+    "revision": "ec191bf862ae4155a01f",
+    "url": "/simple-portfolio-react-app/static/js/runtime-main.e1777f97.js"
+  }
+]);
